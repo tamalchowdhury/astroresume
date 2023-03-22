789e3009e268b7e2e8408e3378a3d67feba66f61
Tamal Chowdhury <tamalanwaronline@gmail.com>
2023-03-22 22:14:44 +0600

    Updates the resume to front end

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tamal Chowdhury';
-export const SITE_DESCRIPTION = 'Tamal Chowdhury is a software engineer and developer advocate from Bangladesh. He is currently looking for his next role in tech. Check out the resume';
+export const SITE_DESCRIPTION = 'Tamal Chowdhury is a software engineer from Bangladesh, specialized in frontend ReactJS. He is currently looking for his next role in tech. Check out the resume';
