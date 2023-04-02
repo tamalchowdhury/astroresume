3eaee4d3433dfad3b8ab9fcf50420a2cea19ce7c
Tamal Chowdhury <tamalanwaronline@gmail.com>
2023-04-02 16:01:48 +0600

    Changes bunch of name and wordings

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tamal Chowdhury';
-export const SITE_DESCRIPTION = 'Tamal Chowdhury is a software engineer from Bangladesh, specialized in frontend ReactJS. He is currently looking for his next role in tech. Check out the resume';
+export const SITE_TITLE = 'Tamal Anwar Chowdhury';
+export const SITE_DESCRIPTION = 'Tamal Anwar Chowdhury is a software engineer from Bangladesh, specialized in frontend ReactJS. He is currently looking for his next role in tech. Check out the resume';
